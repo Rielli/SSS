ef202949e82519d6bd4e8fb59d10a90e95da8ccc
Danila Fedorin <nothingnut@gmail.com>
2014-07-12 22:14:26 -0700

    Shhh I still need to add OpenGL to my eclipse

---
+++ Sunset Space Simulation/src/com/sss/main/SSS.java
@@ -0,0 +1,27 @@
+package com.sss.main;
+
+public class SSS {
+
+	public SSS(){
+		
+	}
+	
+	/**
+	 * Begins the SSS
+	 */
+	public void run(){
+		
+	}
+	
+	/**
+	 * Inits the OpenGL display
+	 */
+	public void createOpenGL(){
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
