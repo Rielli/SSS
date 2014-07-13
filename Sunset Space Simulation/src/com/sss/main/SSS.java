4c377e85b2fe0977edcfe983e9732c4816b7c0c6
Danila Fedorin <nothingnut@gmail.com>
2014-07-12 23:05:08 -0700

    Stuff
    (Added opengl init and file download)

--- Sunset Space Simulation/src/com/sss/main/SSS.java
+++ Sunset Space Simulation/src/com/sss/main/SSS.java
@@ -1,7 +1,16 @@
 package com.sss.main;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import com.sss.util.ResourceUtil;
+
 public class SSS {
 
+	private final int DefaultWidth = 800, DefaultHeight = 600;
+	
 	public SSS(){
 		
 	}
@@ -10,18 +19,32 @@ public class SSS {
 	 * Begins the SSS
 	 */
 	public void run(){
-		
+		ResourceUtil.getResources();
+		createOpenGL();
+		while(!Display.isCloseRequested()){
+			
+		}
+		Display.destroy();
+		System.exit(0);
 	}
 	
 	/**
 	 * Inits the OpenGL display
 	 */
 	public void createOpenGL(){
-		
+		try {
+			Display.setDisplayMode(new DisplayMode(DefaultWidth, DefaultHeight));
+			Display.setTitle("Sunset Space Simulation");
+			Display.create();
+			Keyboard.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
-		
+		SSS sss = new SSS();
+		sss.run();
 	}
 	
 }
